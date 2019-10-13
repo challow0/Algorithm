4e72678e37b6d0276c6f598b52ec649deea16824
Challow <zqm_0011@163.com>
2019-10-13 10:39:21 +0800

    Aquarium

--- STL/平衡二叉树.cpp
+++ STL/平衡二叉树.cpp
@@ -1,143 +1,34 @@
 #include<cstdio>
-const int maxn = 100;
-
+#include<algorithm>
+using namespace std;
 struct node
 {
-    int data;
-    node* lchild;
-    node* rchild;
-}Node[maxn];
-
-void search(node* root,int x)
-{
-    if(root==NULL)
-    {
-        printf("search failed!\n");
-        return ;
-    }
-    if(x == root->data)
-    {
-        printf("%d\n",root->data);
-    }
-    else if(x > root->data)
-    {
-        search(root->rchild,x);
-    }
-    else
-    {
-        search(root->lchild,x);
-    }
-}
-
+    int v, height;
+    node * lchild, * rchild;
+};
 node* newNode(int v)
 {
     node* Node = new node;
-    Node->data = v;
+    Node->v = v;
+    Node->height = 1;
     Node->lchild = Node->rchild = NULL;
     return Node;
 }
 
-void insert (node* &root,int x)
+int getHeight(node* root)
 {
-    if(root == NULL)
-    {
-        root = newNode(x);
-        return ;
-    }
-    if(x == root->data)
-    {
-        return;
-    }
-    else if(x < root->data)
-    {
-        insert(root->lchild,x);
-    }
-    else
-    {
-        insert(root->rchild,x);
-    }
-    
-}
-node* Create(int data[],int n)
-{
-    node* root = NULL;
-    for(int i = 0;i<n;i++)
-        {
-            insert(root,data[i]);
-        }
-    return root;
+    if(root == NULL) return 0;
+    return root->height;
 }
 
-void preorder(node* t)
+int getBalanceFactor(node* root)
 {
-    if(t==NULL)
-        return ;
-    printf("%d\n",t->data);
-    preorder(t->lchild);
-    preorder(t->rchild);
+    return getHeight(root->lchild) - getHeight(root->rchild);
 }
 
-node* findMax(node* root)
+void updateHeight(node* root)
 {
-    while(root->rchild!=NULL)
-    {
-        root = root->rchild;
-    }
-    return root;
-
+    root->height = max(getHeight(root->lchild),getHeight(root->rchild)) +1;
 }
 
-node* findMin(node* root)
-{
-    while(root->lchild!=NULL)
-    {
-        root = root->lchild;
-    }
-    return root;
-}
 
-void deleteNode(node* &root,int x)
-{
-    if(root->data) return;
-    if(root->data == x)
-    {
-        if(root->lchild == NULL && root->rchild == NULL)
-            root = NULL;
-        else if(root->lchild!=NULL)
-        {
-        node* pre = findMax(root->lchild);
-        root->data = pre->data;
-        deleteNode(root->lchild,pre->data);
-        }
-        else
-        {
-        node* next = findMin(root->rchild);
-        root->data = next->data;
-        deleteNode(root->rchild,next->data);
-        }
-    }
-    else if(root->data > x)
-    {
-        deleteNode(root->lchild,x);
-    }
-    else
-    {
-        deleteNode(root->rchild,x);
-    }
-    
-    
-    
-}
-
-int main()
-{
-    int a[7] = {5,3,7,4,2,8,6};
-    int b[7] = {7,4,5,8,2,6,3};
-    node* at = Create(a,7);
-    node* bt = Create(b,7);
-    preorder(at);
-    preorder(bt);
-    getchar();
-    getchar();
-    return 0;
-}
